7339efc8b6269fa038b086e5e30b9ae82891c1e2
melchior2137 <84140494+melchior2137@users.noreply.github.com>
2022-10-25 11:59:14 +0200

    added comments

--- Sorting Algorithms/bubble_sort.cpp
+++ Sorting Algorithms/bubble_sort.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-#include <iterator>
-#include <algorithm>
-#include <vector>
-#include <string>
+#include <iostream>     //std::cout
+#include <iterator>     //std::iterator, std::prev, std::distance
+#include <algorithm>    //std::shuffle, std::swap, std::is_sorted
+#include <vector>       // std::vector
+#include <string>       // std::string
+#include <random>       // std::default_random_engine
+#include <chrono>       // std::chrono::system_clock
 
+
+//custom compare function
 template <typename typ>
 bool comp(const typ &a, const typ &b)
 {
@@ -12,6 +16,7 @@ bool comp(const typ &a, const typ &b)
     return false;
 }
 
+//bubble sort without custom compare function
 template <typename BubbleIterator>
 void bubble_sort(BubbleIterator begin, BubbleIterator end)
 {
@@ -24,6 +29,7 @@ void bubble_sort(BubbleIterator begin, BubbleIterator end)
                 std::swap(*i, *j);
 }
 
+//bubble sort with custom compare function
 template <typename BubbleIterator, typename Compare>
 void bubble_sort(BubbleIterator begin, BubbleIterator end, Compare comp)
 {
@@ -38,6 +44,7 @@ void bubble_sort(BubbleIterator begin, BubbleIterator end, Compare comp)
 
 int main()
 {
+    unsigned seed; //seed for shuffling arrays and vectors
 
     //araray of integers
     int a[] = {25, 262, 534, 396, 385, 237, 2, 63, 12};
@@ -50,6 +57,8 @@ int main()
     std::cout << std::is_sorted(b.begin(), b.end())<<std::endl;
 
     //vector of floats with compare
+    seed = std::chrono::system_clock::now().time_since_epoch().count(); //random seed
+    std::shuffle(b.begin(), b.end(), std::default_random_engine(seed)); //shuffle vector
     bubble_sort(b.begin(), b.end(), comp<float>);
     std::cout << std::is_sorted(b.begin(), b.end(), comp<float>)<<std::endl;
 
@@ -59,6 +68,8 @@ int main()
     std::cout << std::is_sorted(c.begin(), c.end())<<std::endl;
 
     //vector of std::string with compare
+    seed = std::chrono::system_clock::now().time_since_epoch().count(); //random seed
+    std::shuffle(c.begin(), c.end(), std::default_random_engine(seed)); //shuffle vector
     bubble_sort(c.begin(), c.end(), comp<std::string>);
     std::cout << std::is_sorted(c.begin(), c.end(), comp<std::string>)<<std::endl;
 }
--- Sorting Algorithms/quick_sort.cpp
+++ Sorting Algorithms/quick_sort.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-#include <iterator>
-#include <algorithm>
-#include <vector>
-#include <string>
+#include <iostream>     //std::cout
+#include <iterator>     //std::iterator, std::prev, std::distance
+#include <algorithm>    //std::shuffle, std::swap, std::is_sorted
+#include <vector>       // std::vector
+#include <string>       // std::string
+#include <random>       // std::default_random_engine
+#include <chrono>       // std::chrono::system_clock
 
+//custom compare function
 template <typename typ>
 bool comp(const typ &a, const typ &b)
 {
@@ -12,6 +15,7 @@ bool comp(const typ &a, const typ &b)
     return false;
 }
 
+//quick sort without compare
 template <typename RandomIt>
 RandomIt Partition(RandomIt first, RandomIt last)
 {
@@ -40,6 +44,7 @@ void quick_sort(RandomIt first, RandomIt last)
     }
 }
 
+//quick sort with compare
 template <typename RandomIt, typename Compare>
 RandomIt Partition(RandomIt first, RandomIt last, Compare compare)
 {
@@ -71,26 +76,32 @@ void quick_sort(RandomIt first, RandomIt last, Compare compare)
 int main()
 {
 
-    // araray of integers
+    unsigned seed; //seed for shuffling arrays
+
+    //araray of integers
     int a[] = {25, 262, 534, 396, 385, 237, 2, 63, 12};
     quick_sort(std::begin(a), std::end(a));
-    std::cout << std::is_sorted(std::begin(a), std::end(a)) << std::endl;
+    std::cout << std::is_sorted(std::begin(a), std::end(a))<<std::endl;
 
-    // //vector of floats
+    //vector of floats
     std::vector<float> b = {256.333, 26.53, 267.2, 1267.73, 126.732, 173.723, 732.723, 732.125, 111, 10.5};
     quick_sort(b.begin(), b.end());
-    std::cout << std::is_sorted(b.begin(), b.end()) << std::endl;
+    std::cout << std::is_sorted(b.begin(), b.end())<<std::endl;
 
-    // vector of floats with compare
+    //vector of floats with compare
+    seed = std::chrono::system_clock::now().time_since_epoch().count(); //random seed
+    std::shuffle(b.begin(), b.end(), std::default_random_engine(seed)); //shuffle vector
     quick_sort(b.begin(), b.end(), comp<float>);
-    std::cout << std::is_sorted(b.begin(), b.end(), comp<float>) << std::endl;
+    std::cout << std::is_sorted(b.begin(), b.end(), comp<float>)<<std::endl;
 
-    // vector of std::string
+    //vector of std::string
     std::vector<std::string> c = {"abc", "a", "polska", "andrzej", "ratata", "okrag", "b"};
     quick_sort(c.begin(), c.end());
-    std::cout << std::is_sorted(c.begin(), c.end()) << std::endl;
+    std::cout << std::is_sorted(c.begin(), c.end())<<std::endl;
 
-    // vector of std::string with compare
+    //vector of std::string with compare
+    seed = std::chrono::system_clock::now().time_since_epoch().count(); //random seed
+    std::shuffle(c.begin(), c.end(), std::default_random_engine(seed)); //shuffle vector
     quick_sort(c.begin(), c.end(), comp<std::string>);
-    std::cout << std::is_sorted(c.begin(), c.end(), comp<std::string>) << std::endl;
+    std::cout << std::is_sorted(c.begin(), c.end(), comp<std::string>)<<std::endl;
 }
